c5ea06ebae1fda17bf78577129a416a17913f01d
msaidzengin <m.zengin@etu.edu.tr>
2020-04-23 00:22:33 +0300

    only urls

--- scan.py
+++ scan.py
@@ -5,18 +5,22 @@ import time
 url = 'https://urlscan.io/api/v1/search/?q='
 words = ["banka", "devlet", "korona"]
 results = {}
-result_list = []
+result_url_list = {}
 
 for word in words:
     r = requests.get(url + word)
     if r.status_code == 200:
         data = r.json()
         results[word] = data['results']
-        result_list += data['results']
+        urls = []
+        for res in data['results']:
+            urls.append(res['task']['url'])
+        result_url_list[word] = urls
         print(word + " | sonuc sayisi: " + str(len(data['results'])))
         time.sleep(2)
 
+
 with open('result.json', 'w', encoding='utf-8') as f:
-    json.dump(results, f, ensure_ascii=False, indent=4)
+    json.dump(result_url_list, f, ensure_ascii=False, indent=4)
 
 print("result.json kaydedildi.")
